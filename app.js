bf963e85e43e1cc9b3daf3e81aa0989547876653
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-20 13:19:34 +0900

    Feature: hold 버튼을 클릭했을때 player의 realScore에 주사위 눈의 합을 계산하여 표시해 주는 기능 개발

--- app.js
+++ app.js
@@ -44,3 +44,54 @@ rollDiceButton.addEventListener("click", (event) => {
         }
     }   
 });
+
+// hold 버튼을 클릭하면 각 player의 real-score에 점수를 적재한다
+// 발생한 문제상황
+/**
+// 발생한 문제상황 
+ * 상대방이 hold를 한후 내가 주사위를 돌려서 1이나왔을때
+ *  currentScore = 0
+ * 다시 내 차례가 되어 주사위를 돌렸는데 4가 나왔을때
+ * realScore 기존값 + 4가 되어야 한다 
+ * 
+ * 발생한 문제상황
+ * hold를 누르면 currentScore = 0이 되어야 한다
+ * 왜? currentScore가 0이 되지 않으면 다음에 다시 내 차례가 되어 주사위를 돌렸을때 이전 currentScore += 주사위값이 되어 realScore가 잘못계산되기 때문이다
+ * 
+ * 발생했던 문제
+ * innerHTML에서 가져온 값의 타입이 String이여서 문자가 그대로 더해지는 문제가 있었다
+ * 이를 해결하기 위해 innerHTML 값을 number 타입으로 형변환 해줘야 한다
+ */
+holdDiceButton.addEventListener('click', (event) => {
+    if (turnFlag == 1) {
+        // 기존 realScore를 가져온다
+        playerOneRealScoreTemp = Number(playerOneRealScoreSelector.innerHTML);
+        playerOneRealScoreTemp += player1CurrentScore;
+        // realScore에 세팅해준다
+        playerOneRealScoreSelector.innerHTML = playerOneRealScoreTemp;
+        player1CurrentScore = 0;
+        playerOneCurrentScoreSelector.innerHTML = player1CurrentScore;
+
+        if (playerOneRealScoreTemp == 50) {
+            alert("player1 승리");
+            resetNewGame();
+        }
+
+        turnFlag = 0;
+    } else {
+        // 기존 realScore를 가져온다
+        playerTwoRealScoreTemp = Number(playerTwoRealScoreSelector.innerHTML);
+        playerTwoRealScoreTemp += player2CurrentScore;
+        // realScore에 세팅해준다
+        playerTwoRealScoreSelector.innerHTML = playerTwoRealScoreTemp;
+        player2CurrentScore = 0;
+        playerTwoCurrentScoreSelector.innerHTML = player2CurrentScore;
+        
+        if (playerTwoRealScoreTemp == 50) {
+            alert("player2 승리");
+            resetNewGame();
+        }
+
+        turnFlag = 1;
+    } 
+});
